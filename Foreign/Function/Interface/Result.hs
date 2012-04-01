9f43acb1621ab3f92c4fcab98fba74f3d2918b11
Edward Kmett <ekmett@gmail.com>
2012-04-01 01:33:39 -0400

    getting results

---
+++ Foreign/Function/Interface/Result.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE ExistentialTypes #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE Rank2Types #-}
+module Foreign.Function.Interface.Result
+  ( Result(..)
+  ) where
+
+import Foreign.Ptr
+import Foreign.Function.Interface.Type
+
+data Result a = forall x. Result
+  { resultType   :: !Type
+  , resultNew    :: IO (Ptr x)
+  , resultFinish :: forall r. (a -> Ptr x -> IO r) -> IO r
+  }
+
+instance Functor Result where
+  fmap f (Result t new kf) = Result t new (\k -> kf (k . f))
+
